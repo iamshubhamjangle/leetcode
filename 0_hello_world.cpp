15989d02e3084d9367b55ff2e0183895c29549b2
Shubham Jangle <jangleshubham13@gmail.com>
2022-01-25 21:00:32 +0530

    :)

---
+++ 0_hello_world.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+
+int main(){
+	int t;
+	cin >> t;		//input no. of test cases
+	while(t--){
+		solve();	//run this 't' times
+	}
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << n << endl;
+}
