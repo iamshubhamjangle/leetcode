3ce35b6d69cb4b4f00d81073b66dfa58ff6ff7c4
Shubham Jangle <jangleshubham13@gmail.com>
2022-05-28 00:28:31 +0530

    :)

--- 57_Search_Pattern_Rabin_Karp_Algorithm.cpp
+++ 57_Search_Pattern_Rabin_Karp_Algorithm.cpp
@@ -12,6 +12,9 @@ Input : S = "abesdu", pat = "edu"
 Output: -1
 
 https://practice.geeksforgeeks.org/problems/31272eef104840f7430ad9fd1d43b434a4b9596b/1/
+	Method 1: loop through txt and match pat. TC: O(N). SC: O(N)
+    	- if(first letter of pat == txt) then get txt substr(i, lengthOfPat) == pat
+    	- if yes move the index to vector or else at end move -1 of vector empty
 */
 
 #include<bits/stdc++.h>
@@ -20,11 +23,28 @@ using namespace std;
 
 class Solution {
     public:
-        vector<int> search(string pat, string txt)
-        {
-            //code here.
+    	// Method 1: loop through txt. TC: O(N). SC: O(N)
+    	// - if(first letter of pat == txt) then get txt substr(i, lengthOfPat) == pat
+    	// - if yes move the index to vector or else at end move -1 of vector empty
+		vector<int> search(string pat, string txt) {
+        	int n = txt.size();
+        	int m = pat.size();
+        	vector<int> ans;
+
+			for(int i=0; i<n; i++) {
+				if(txt[i] == pat[0]) {
+					if(txt.substr(i,m) == pat) {
+						// cout << "sub: " << txt.substr(i,m) << " == " << pat << endl;
+						ans.push_back(i+1);
+					}
+				}
+			}
+
+			if(!ans.size()) ans.push_back(-1);
+
+			return ans;
         }
-     
+
 };
 
 int main() {
