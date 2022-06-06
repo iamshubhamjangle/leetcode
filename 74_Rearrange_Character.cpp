926712f453310de9b7f72ae38a8891bbbab56b40
Shubham Jangle <jangleshubham13@gmail.com>
2022-06-06 23:51:23 +0530

    :)

---
+++ 74_Rearrange_Character.cpp
@@ -0,0 +1,113 @@
+/*
+Rearrange_Character.cpp
+
+Constraints:
+1 <= T <= 100
+1 <= length of string <= 104
+
+Example:
+Input:
+3
+geeksforgeeks
+bbbabaaacd
+bbbbb
+
+Output:
+1
+1
+0
+
+https://practice.geeksforgeeks.org/problems/rearrange-characters4649/1/#
+	- Method 1: using map. TC: O(n)  SC: O(26)	
+*/
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+const int MAX_CHAR = 26;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    string rearrangeString(string str)
+    {
+        map<char,int> mp;
+       int c1=0;
+       for(auto c:str)
+       {
+           mp[c]++;
+           c1=max(c1,mp[c]);
+       }
+       if(c1>(str.length()+1)/2)return "-1";
+       int start=0;
+       for(auto x: mp)
+       {
+           while(x.second--)
+           {
+               str[start]=x.first;
+               start+=2;
+               if(start>=str.length())start=1;
+           }
+       }
+       return str;
+    }
+    
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin>>str;
+	    Solution ob;
+	    string str1=ob.rearrangeString(str);
+	    int flag=1;
+	    int c[26] = {0};
+	    for(int i=0; i<str.length(); i++)
+	        c[str[i]-'a']+=1;
+	    int f = 0;
+	    int x = (str.length()+1)/2;
+	    for(int i=0; i<26; i++)
+	    {
+	        if(c[i]>x)
+	            f = 1;
+	    }
+	    if(f)
+	    {
+	        if(str1=="-1")
+	            cout<<0<<endl;
+	        else
+	            cout<<1<<endl;
+	    }
+	    else
+	    {
+    	    int a[26] = {0};
+    	    int b[26] = {0};
+    	    for(int i=0; i<str.length(); i++)
+    	        a[str[i]-'a']+=1;
+    	    for(int i=0; i<str1.length(); i++)
+    	        b[str1[i]-'a']+=1;
+    	        
+    	    for(int i=0; i<26; i++)
+    	        if(a[i]!=b[i])
+    	            flag = 0;
+    	    
+    	    for(int i=0;i<str1.length();i++)
+    	    {
+    		    if(i>0)
+    		        if(str1[i-1]==str1[i])
+    			        flag=0;
+    	    }
+    	    if(flag==1)
+    		    cout<<"1\n";
+    	    else
+    	        cout<<"0\n";
+	    }
+	}
+    return 0;
+}  // } Driver Code Ends
