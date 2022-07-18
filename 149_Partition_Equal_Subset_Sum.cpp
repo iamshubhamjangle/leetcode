9da09d2961ba8ad4017f86bf03f01df762263d94
Shubham Jangle <jangleshubham13@gmail.com>
2022-07-18 22:36:31 +0530

    :)

--- 149_Partition_Equal_Subset_Sum.cpp
+++ 149_Partition_Equal_Subset_Sum.cpp
@@ -11,7 +11,8 @@ Input: N = 3, arr = {1, 3, 5}
 Output: NO
 
 https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1
-
+Method 1: Bruteforce TC: O(2^n) SC: O(N) - stack
+Method 2: DP (knapsack similar) TC: O(N*sum) SC: O(N*sum)
 */
 
 #include <bits/stdc++.h>
@@ -19,13 +20,46 @@ using namespace std;
 
 class Solution{
 public:
-    int equalPartition(int n, int arr[]) {
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		if(i==j) continue;
+    // Bruteforce TLE: TC: O(2^n) SC: O(N) - stack space 
+    /*
+    bool isEqualSumSubset(int arr[], int n, int target) {
+        if(n == 0) return target == 0;      // return true if target is zero
 
-        	}
-        } 
+        bool take, dontTake;
+        take = isEqualSumSubset(arr, n-1, target - arr[n]);
+        dontTake = isEqualSumSubset(arr, n-1, target);
+
+        return take || dontTake;
+    }
+
+    int equalPartition(int n, int arr[]) {
+        int totalSum = accumulate(arr, arr+n, 0);
+        if(totalSum & 1) return 0;
+        return isEqualSumSubset(arr, n-1, totalSum/2);
+    }
+    */
+
+
+    // Following knapsack pattern
+    int equalPartition(int n, int arr[]) {
+        int sum = accumulate(arr, arr+n, 0);                // calculate sum of arr
+        if(sum & 1) return 0;                               // if odd
+
+        bool dp[n+1][sum+1];                                // initialize dp
+        for(int i=0; i<=sum; i++) { dp[0][i] = false; }     // First row to false
+        for(int i=0; i<=n; i++) { dp[i][0] = true; }        // First column to true;
+
+        for(int i=1; i<=n; i++) {                           // row
+            for(int j=1; j<=sum; j++) {                     // column
+                if(arr[i-1] <= j){
+                    dp[i][j] = (dp[i-1][j] || dp[i-1][j-arr[i-1]]);
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][sum/2];
     }
 };
 
---
+++ 154_M-Coloring_Problem.cpp
@@ -0,0 +1,84 @@
+/*
+M-Coloring_Problem.cpp
+
+Given an undirected graph and an integer M. The task is to determine if the graph can be 
+colored with at most M colors such that no two adjacent vertices of the graph are colored 
+with the same color. Here coloring of a graph means the assignment of colors to all vertices. 
+Print 1 if it is possible to colour vertices and 0 otherwise.
+
+Input:
+N = 4
+M = 3
+E = 5
+Edges[] = {(0,1),(1,2),(2,3),(3,0),(0,2)}
+Output: 1
+
+Input:
+N = 3
+M = 2
+E = 3
+Edges[] = {(0,1),(1,2),(0,2)}
+Output: 0
+
+https://practice.geeksforgeeks.org/problems/m-coloring-problem-1587115620/1
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+	bool isPossible(bool graph[101][101],int color[],int N,int col,int node){
+        for(int k=0;k<N;k++){
+            if(k!=node && graph[node][k]==1 && color[k]==col) return false;
+        }
+        return true;
+    }
+    
+    bool solve(bool graph[101][101],int m,int N,int color[],int node){
+        if(node==N) return true;
+            
+        // traverse onto diff color recursively
+        for(int i=1;i<=m;i++){  
+            if(isPossible(graph,color,N,i,node)){
+                color[node]=i;
+                if(solve(graph,m,N,color,node+1)) return true;
+                color[node]=0;
+            }
+                
+        }
+        return false;
+    }
+    
+    bool graphColoring(bool graph[101][101], int m, int N) {
+        int color[N];
+        memset(color,0,sizeof color);
+        return solve(graph,m,N,color,0);
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, e;
+        cin >> n >> m >> e;
+        int i;
+        bool graph[101][101];
+        for (i = 0; i < n; i++) {
+            memset(graph[i], 0, sizeof(graph[i]));
+        }
+        for (i = 0; i < e; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a - 1][b - 1] = 1;
+            graph[b - 1][a - 1] = 1;
+        }
+        Solution ob;
+        cout << ob.graphColoring(graph, m, n) << endl;
+    }
+    return 0;
+}
