5f3c41b67a51d5010f006f975ec7427b47dccdbf
Shubham Jangle <jangleshubham13@gmail.com>
2022-08-06 13:13:05 +0530

    :)

--- 190_Number_Of_Subsets.cpp
+++ 190_Number_Of_Subsets.cpp
@@ -47,7 +47,7 @@ using namespace std;
 // }
 
 
-// // Memoisation. TC: O(N*target) SC: O(N) ASC: O(N)
+// // Memoisation. TC: O(N*target) SC: O(N*target) ASC: O(N)
 // int solve(int ind, int sum, vector<int> &num, vector<vector<int>> &dp) {
 // 	if(ind == 0) {
 // 		if(sum == 0 && num[ind] == 0) return 2;
@@ -69,6 +69,56 @@ using namespace std;
 // 	vector<vector<int>> dp(n, vector<int> (tar+1, -1));
 // 	return solve(n-1, tar, num, dp);
 // }
+
+
+// // Tabulation. Top-Down - 0 to n-1. TC: O(N*target) SC: O(N*target)
+// int findWays(vector<int> &num, int sum) {
+// 	int n = num.size();
+// 	vector<vector<int>> dp(n, vector<int> (sum+1, 0));
+
+// 	if(num[0] == 0) dp[0][0] = 2;	// if(ind == 0) { if(sum == 0 && num[ind] == 0) return 2; }
+// 	else dp[0][0] = 1;				// if(ind == 0) { sum == 0 || num[ind] == sum } return 1; }
+
+// 	if(num[0] != 0 && num[0] <= sum) dp[0][num[0]] = 1;
+
+// 	for(int ind=1; ind<n; ind++) {
+// 		for(int j=0; j<=sum; j++) {
+// 			int notake = dp[ind-1][j];
+// 			int take = 0;
+// 			if(num[ind] <= j) take = dp[ind-1][j - num[ind]];
+
+// 			dp[ind][j] = take + notake;
+// 		}
+// 	}
+
+// 	return dp[n-1][sum];
+// }
+
+
+
+// Space Optimisation. TC: O(N*target) SC: O(target)
+int findWays(vector<int> &num, int sum) {
+	int n = num.size();
+	vector<int> prev(sum+1, 0), curr(sum+1, 0);
+
+	if(num[0] == 0) prev[0] = 2;	// if(ind == 0) { if(sum == 0 && num[ind] == 0) return 2; }
+	else prev[0] = 1;				// if(ind == 0) { sum == 0 || num[ind] == sum } return 1; }
+
+	if(num[0] != 0 && num[0] <= sum) prev[num[0]] = 1;
+
+	for(int ind=1; ind<n; ind++) {
+		for(int j=0; j<=sum; j++) {
+			int notake = prev[j];
+			int take = 0;
+			if(num[ind] <= j) take = prev[j - num[ind]];
+
+			curr[j] = take + notake;
+		}
+		prev = curr;
+	}
+
+	return prev[sum];
+}
 
 
 
---
+++ 191_Partitions_With_Given_Difference.cpp
@@ -0,0 +1,22 @@
+/*
+Partitions_With_Given_Difference.cpp
+
+Input:
+2
+4 3
+5 2 6 4
+4 0
+1 1 1 1
+
+Output:
+1
+6
+
+https://www.codingninjas.com/codestudio/problems/partitions-with-given-difference_3751628
+
+*/
+
+
+
+
+
