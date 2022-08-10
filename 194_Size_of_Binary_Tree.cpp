92c4a5b8f8dfd76096f4751b585c8f58faa5e3ac
Shubham Jangle <jangleshubham13@gmail.com>
2022-08-10 23:32:34 +0530

    :)

--- 194_Size_of_Binary_Tree.cpp
+++ 194_Size_of_Binary_Tree.cpp
@@ -123,9 +123,15 @@ int main()
 
 
 
-// Your Code Here
+// Recursion
 int getSize(Node* node) {
    if(node == NULL) return 0;
    
    return 1 + getSize(node -> left) + getSize(node -> right);
+}
+
+
+// When there is no left / right child we will not make the call
+int getSize(Node* node) {
+   return 1 + (node->left ? getSize(node->left) : 0) + node->right ? (getSize(node->right) : 0);
 }
