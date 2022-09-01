3cb95e2d6d331697262434fc0cfcbfc84f2eefe4
Shubham Jangle <jangleshubham13@gmail.com>
2022-09-01 22:38:43 +0530

    :)

--- 243_Minimum_Amount_of_Time_to_Collect_Garbage.cpp
+++ 243_Minimum_Amount_of_Time_to_Collect_Garbage.cpp
@@ -42,17 +42,23 @@ using namespace std;
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int last[128] = {}, res = 0;
+        int last[128] = {}, res = 0; // map of characters
+
+        // Input: garbage = ["G","P","GP","GG"], travel = [2,4,3]
+        // Output: 21
         for (int i = 0; i < garbage.size(); ++i) {
             res += garbage[i].size();
             for (char c : garbage[i])
                 last[c] = i;
         }
+
         for (int j = 1; j < travel.size(); ++j)
             travel[j] += travel[j - 1];
+
         for (int c : "PGM")
             if (last[c])
                 res += travel[last[c] - 1];
+
         return res;
     }
 };
---
+++ 244_Vertical_Order_Traversal_of_a_Binary_Tree.cpp
@@ -0,0 +1,25 @@
+/**
+Vertical_Order_Traversal_of_a_Binary_Tree.cpp
+
+Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+
+Return the vertical order traversal of the binary tree.
+
+https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        
+    }
+};
