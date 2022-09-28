6bf7f38afc7da9babc6a4a91be6447d540140d49
Shubham Jangle <jangleshubham13@gmail.com>
2022-09-28 23:50:25 +0530

    :)

---
+++ 322_Detect_cycle_in_an_undirected_graph.cpp
@@ -0,0 +1,44 @@
+/*
+Detect_cycle_in_an_undirected_graph.cpp
+
+https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+*/
+
+class Solution {
+    public:
+    // TC: O(N+E) SC: O(N+E)
+    bool checkForCycle(int s, int V, vector<int> adj[], vector<int> &visited) {
+        queue<pair<int, int>> q;
+        visited[s] = true;
+        q.push({s, -1});
+        while (!q.empty()) {
+            int node = q.front().first;
+            int par = q.front().second;
+            q.pop();
+ 
+            for (auto it : adj[node]) {
+                if (!visited[it]) {
+                    visited[it] = true;
+                    q.push({it, node});
+                }
+                else if (par != it)
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    bool isCycle(int V, vector<int> adj[]){
+        vector<int> vis(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (checkForCycle(i, V, adj, vis))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+};
