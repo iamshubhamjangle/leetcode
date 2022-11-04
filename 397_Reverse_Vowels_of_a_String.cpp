4ed13af3560d7e9013f58295363b34c15fb0a9ed
Shubham Jangle <jangleshubham13@gmail.com>
2022-11-05 00:13:10 +0530

    :)

--- 397_Reverse_Vowels_of_a_String.cpp
+++ 397_Reverse_Vowels_of_a_String.cpp
@@ -6,7 +6,7 @@ https://leetcode.com/problems/reverse-vowels-of-a-string/
 
 class Solution {
 public:
-	// TC: O(2N) SC: O(N)
+	// 1. Using Stack, Set. Runtime: 30ms. TC: O(2N) SC: O(N)
     string _reverseVowels(string s) {
         unordered_set<char> us = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
         stack<char> st;
@@ -28,8 +28,8 @@ public:
     }
 
 
-    // TC: O(N) SC: O(1)
-    string reverseVowels(string s) {
+    // 2. Two pointer, Set. Runtime: 18ms. TC: O(N) SC: O(1)
+    string _reverseVowels(string s) {
         unordered_set<char> us = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
     	int n = s.size();
 
@@ -41,12 +41,32 @@ public:
     			i++;
     		else if(us.find(s[j]) == us.end())
     			j--;
-    		else{
-    			swap(s[i], s[j]);
-    			i++;
-    			j--;
-    		}
+    		else
+    			swap(s[i++], s[j--]);
     	}
+
+        return s;
+    }
+
+
+    // 3. Runtime: 4ms. TC: O(N) SC: O(1)
+    bool isVowel(char c) const{
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || 
+            c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
+    }
+
+    string reverseVowels(string s) {
+        int i = 0;
+        int j = s.size()-1;
+
+        while(i<j) {
+            if(!isVowel(s[i]))
+                i++;
+            else if(!isVowel(s[j]))
+                j--;
+            else
+                swap(s[i++], s[j--]);
+        }
 
         return s;
     }
